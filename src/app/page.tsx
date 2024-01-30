ae2a10d81be6bb11143570586ec4686a7fa2b7ca
yikejason <18215513386@163.com>
2024-01-30 14:45:39 +0800

    added work Experience component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { Button } from "@/components/ui/button";
 import { MailIcon, PhoneIcon, GlobeIcon } from "lucide-react";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { Section } from "@/components/ui/section";
+import { Card, CardHeader, CardContent } from "@/components/ui/card";
+import { MY_RESUME_DATA } from "@/data/my-resume-data";
 
 export default function Home() {
   return (
@@ -9,9 +11,9 @@ export default function Home() {
       <section className="mx-auto w-full max-w-2xl space-y-8 bg-white print:space-y-6">
         <div className="flex items-center justify-between">
           <div className="flex-1 space-y-1.5">
-            <h1 className="text-2xl font-bold">天天</h1>
+            <h1 className="text-2xl font-bold">{MY_RESUME_DATA.name}</h1>
             <p className="max-w-md text-pretty font-mono text-sm text-muted-foreground">
-              我是一名前端工程师, 目前工作在成都, 如果看到我的简历请联系我
+              {MY_RESUME_DATA.about}
             </p>
             <p className="max-w-md items-center text-pretty font-mono text-xs text-muted-foreground">
               <a
@@ -20,17 +22,17 @@ export default function Home() {
                 target="_blank"
               >
                 <GlobeIcon className="size-3" />
-                四川,成都
+                {MY_RESUME_DATA.location}
               </a>
             </p>
             <div className="flex gap-x-1 pt-1 font-mono text-sm text-muted-foreground print:hidden">
               <Button className="size-8" variant="outline" size="icon">
-                <a href={`mailto:18215513386@163.com`}>
+                <a href={`mailto:${MY_RESUME_DATA.email}`}>
                   <MailIcon className="size-4" />
                 </a>
               </Button>
               <Button className="size-8" variant="outline" size="icon" asChild>
-                <a href={`tel:18215513386`}>
+                <a href={`tel:${MY_RESUME_DATA.tel}`}>
                   <PhoneIcon className="size-4" />
                 </a>
               </Button>
@@ -38,16 +40,43 @@ export default function Home() {
           </div>
 
           <Avatar className="size-28">
-            <AvatarImage alt="jason" src="https://github.com/shadcn.png" />
-            <AvatarFallback>JA</AvatarFallback>
+            <AvatarImage alt="jason" src={MY_RESUME_DATA.avatarUrl} />
+            <AvatarFallback>{MY_RESUME_DATA.avatarInit}</AvatarFallback>
           </Avatar>
         </div>
         <Section>
           <h2 className="text-xl font-bold">关于我</h2>
           <p className="text-pretty font-mono text-sm text-muted-foreground">
-            作为一名前端工程师，我已经从0到1开发了多个产品。我领导过团队，构建过最好的工作环境，目前，我使用TypeScript，React，和
-            Node.js。我已经拥有了6年的开发经验在前端开发的工作上。所以我能够胜任前端任何方面的工作。
+            {MY_RESUME_DATA.summary}
           </p>
+        </Section>
+        <Section>
+          <h2 className="text-xl font-bold">工作经验</h2>
+          {MY_RESUME_DATA.workExperience.map((work) => {
+            return (
+              <Card key={work.company}>
+                <CardHeader>
+                  <div className="flex items-center justify-between gap-x-2 text-base">
+                    <h3 className="inline-flex items-center justify-center gap-x-1 font-semibold leading-none">
+                      <a className="hover:underline" href={work.link}>
+                        {work.company}
+                      </a>
+                    </h3>
+                    <div className="text-sm tabular-nums text-gray-500">
+                      {work.start} - {work.end}
+                    </div>
+                  </div>
+
+                  <h4 className="font-mono text-sm leading-none">
+                    {work.title}
+                  </h4>
+                </CardHeader>
+                <CardContent className="mt-2 text-xs">
+                  {work.description}
+                </CardContent>
+              </Card>
+            );
+          })}
         </Section>
       </section>
     </main>
--- src/data/my-resume-data.tsx
+++ src/data/my-resume-data.tsx
@@ -1,4 +1,49 @@
 export const MY_RESUME_DATA = {
   name: "天天",
   about: "我是一名前端工程师, 目前工作在成都, 如果看到我的简历请联系我",
+  location: "四川,成都",
+  email: "18215513386@163.com",
+  tel: "18215513386",
+  avatarUrl: "https://github.com/shadcn.png",
+  avatarInit: "JA",
+  summary:
+    "作为一名前端工程师，我已经从0到1开发了多个产品。我领导过团队，构建过最好的工作环境，目前，我使用TypeScript，React，和Node.js。我已经拥有了6年的开发经验在前端开发的工作上。所以我能够胜任前端任何方面的工作。",
+  workExperience: [
+    {
+      company: "新希望乳业",
+      link: "https://www.newhopedairy.cn",
+      title: "web前端开发工程师",
+      start: "2022",
+      end: "2024",
+      description:
+        "实施新功能，领导团队，致力于改进开发人员发送代码的方式，开始从 Emotion 迁移到 Tailwind CSS 等。技术： React、TypeScript、GraphQL",
+    },
+    {
+      company: "博彦科技",
+      link: "https://www.newhopedairy.cn",
+      title: "web前端开发工程师",
+      start: "2022",
+      end: "2019",
+      description:
+        "实施新功能，领导团队，致力于改进开发人员发送代码的方式，开始从 Emotion 迁移到 Tailwind CSS 等。技术： React、TypeScript、GraphQL",
+    },
+    {
+      company: "成都燃光科技",
+      link: "https://www.newhopedairy.cn",
+      title: "web前端开发工程师",
+      start: "2018",
+      end: "2019",
+      description:
+        "实施新功能，领导团队，致力于改进开发人员发送代码的方式，开始从 Emotion 迁移到 Tailwind CSS 等。技术： React、TypeScript、GraphQL",
+    },
+    {
+      company: "成都福豆科技",
+      link: "https://www.newhopedairy.cn",
+      title: "web前端开发工程师",
+      start: "2017",
+      end: "2018",
+      description:
+        "实施新功能，领导团队，致力于改进开发人员发送代码的方式，开始从 Emotion 迁移到 Tailwind CSS 等。技术： React、TypeScript、GraphQL",
+    },
+  ],
 };
